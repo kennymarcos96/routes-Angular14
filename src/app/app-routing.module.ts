68fdc5e93d1ced5ac300deeb0367cc2bfebd833e
KennyMarcos <101741866+KennyMarcos@users.noreply.github.com>
2022-09-22 09:56:05 -0300

    Rotas Criadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{
+//Pages:
+import { HomeComponent } from './pages/home/home.component';
+import { PageErrorComponent } from './pages/page-error/page-error.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
-}];
+
+const routes: Routes = [
+
+  {path:'', component: HomeComponent , pathMatch:'full'},
+  {path:'sobre', component: SobreComponent },
+  {path:'404', component: PageErrorComponent }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
